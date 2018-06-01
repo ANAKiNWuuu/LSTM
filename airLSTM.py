3c188a6ed8b321be239d76704001657bddbf095b
ANAKiNWuuu <a50911@hotmail.com>
2018-06-01 16:50:35 +0800

    Update and rename airdatatotime.py to airLSTM.py

--- airLSTM.py
+++ airLSTM.py
@@ -11,7 +11,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
  
-# 将序列转换为监督学习问题
+# 將序列轉換為監督學習問題
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
     n_vars = 1 if type(data) is list else data.shape[1]
     df = DataFrame(data)
@@ -35,64 +35,65 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
         agg.dropna(inplace=True)
     return agg
  
-# 加载数据集
+# 數據集
 dataset = read_csv('pollution.csv', header=0, index_col=0)
 values = dataset.values
-# 整数编码
+# 整數編碼
 encoder = LabelEncoder()
 values[:,4] = encoder.fit_transform(values[:,4])
-# 确保所有数据是浮动的
+# 確保所有數據是浮動的
 values = values.astype('float32')
-# 归一化特征
+# 歸一化特徵
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled = scaler.fit_transform(values)
-# 指定滞后时间大小
+# 指定滯後時間大小
 n_hours = 3
 n_features = 8
-# 构建监督学习问题
+# 構建監督學習問題
 reframed = series_to_supervised(scaled, n_hours, 1)
 print(reframed.shape)
  
-# 分为训练集和测试集
+# 分為訓練集和測試集
 values = reframed.values
 n_train_hours = 365 * 24
 train = values[:n_train_hours, :]
 test = values[n_train_hours:, :]
-# 分为输入和输出
+# 分為輸入和輸出
 n_obs = n_hours * n_features
 train_X, train_y = train[:, :n_obs], train[:, -n_features]
 test_X, test_y = test[:, :n_obs], test[:, -n_features]
 print(train_X.shape, len(train_X), train_y.shape)
-# 重塑为3D形状 [samples, timesteps, features]
+# 重塑為3D形狀
+ [samples, timesteps, features]
 train_X = train_X.reshape((train_X.shape[0], n_hours, n_features))
 test_X = test_X.reshape((test_X.shape[0], n_hours, n_features))
 print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
  
-# 设计网络
+# 設計網絡
 model = Sequential()
 model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
 model.add(Dense(1))
 model.compile(loss='mae', optimizer='adam')
-# 拟合网络模型
+# 擬合網絡模型
 history = model.fit(train_X, train_y, epochs=55, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
-# 作出预测
+# 作出預測
 yhat = model.predict(test_X)
 test_X = test_X.reshape((test_X.shape[0], n_hours*n_features))
-# 反向转换预测值比例
+# 反向轉換預測值比例
 inv_yhat = concatenate((yhat, test_X[:, -7:]), axis=1)
 inv_yhat = scaler.inverse_transform(inv_yhat)
 inv_yhat = inv_yhat[:,0]
-# 反向转换实际值大小
+# 反向轉換實際值大小
 test_y = test_y.reshape((len(test_y), 1))
 inv_y = concatenate((test_y, test_X[:, -7:]), axis=1)
 inv_y = scaler.inverse_transform(inv_y)
 inv_y = inv_y[:,0]
-# 计算RMSE大小
+# 計算RMSE大小
 rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
-#列印數據
+# 列印数据
 print('Test RMSE: %.3f' % rmse)
 print(inv_y[:10])
-# 绘制历史数据
+# 繪制歷史數據
 pyplot.plot(history.history['loss'], label='train')
 pyplot.plot(history.history['val_loss'], label='test')
 pyplot.legend()
